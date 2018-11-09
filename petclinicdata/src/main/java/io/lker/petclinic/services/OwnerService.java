acb21d356d304b12fcb4cefe5759cc6ec29eb8e1
ryan <ryantwalk@gmail.com>
2018-11-09 14:47:01 -0500

    refactor to common interfaces. Closes #11.

--- petclinicdata/src/main/java/io/lker/petclinic/services/OwnerService.java
+++ petclinicdata/src/main/java/io/lker/petclinic/services/OwnerService.java
@@ -2,12 +2,8 @@ package io.lker.petclinic.services;
 
 import io.lker.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findbyId(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
