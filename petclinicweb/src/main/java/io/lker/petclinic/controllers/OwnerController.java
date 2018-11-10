460e87e7a25105253fc39465846c6217f81c21e1
Ryan <ryantwalk@gmail.com>
2018-11-10 09:40:05 -0500

    implement owner controller & owner page. Closes #13.

---
+++ petclinicweb/src/main/java/io/lker/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package io.lker.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOfOwners(){
+
+        return "owners/index";
+    }
+}
