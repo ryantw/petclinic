946868745e13fffd51a6e505be6b12cad23c87a2
ryan <ryantwalk@gmail.com>
2018-11-20 18:47:59 -0500

    constructor inject vetservice into vet controller and list all vets on vet page. Closes #15.

--- petclinicweb/src/main/java/io/lker/petclinic/controllers/VetController.java
+++ petclinicweb/src/main/java/io/lker/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package io.lker.petclinic.controllers;
 
+import io.lker.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
