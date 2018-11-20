c15c3b7174865c7ab78bfd3e8dccad7b3f3e3458
ryan <ryantwalk@gmail.com>
2018-11-20 18:38:08 -0500

    list owners on owners pages. Closes #16.

--- petclinicweb/src/main/java/io/lker/petclinic/controllers/OwnerController.java
+++ petclinicweb/src/main/java/io/lker/petclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package io.lker.petclinic.controllers;
 
+import io.lker.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    // final so never changed
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+        // "owners" mapped to a variable, read by thymeleaf template.
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
