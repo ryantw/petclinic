6aa9d290ce2f644bb12fd496f7db7acef1dec9e5
Ryan <ryantwalk@gmail.com>
2018-12-21 21:43:24 -0500

    convert owners and other classes to jpa entities. Closes #36.

--- petclinicdata/src/main/java/io/lker/petclinic/model/Pet.java
+++ petclinicdata/src/main/java/io/lker/petclinic/model/Pet.java
@@ -1,11 +1,25 @@
 package io.lker.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    // Relates back to Owner mapping
+    // 'owner' declared by mapping from Owner.java
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
