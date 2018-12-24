2278d1778d68df4e6e5c71c48c098d3d0992e130
Ryan <ryantwalk@gmail.com>
2018-12-24 16:35:05 -0500

    created visit map service. Closes #46.

--- petclinicdata/src/main/java/io/lker/petclinic/repositories/specialtiesRepository.java
+++ petclinicdata/src/main/java/io/lker/petclinic/repositories/specialtiesRepository.java
@@ -3,5 +3,5 @@ package io.lker.petclinic.repositories;
 import io.lker.petclinic.model.Specialty;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+public interface specialtiesRepository extends CrudRepository<Specialty, Long> {
 }
---
+++ petclinicdata/src/main/java/io/lker/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package io.lker.petclinic.services;
+
+import io.lker.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
