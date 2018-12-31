0c6e8e280adb9c7a053f4ddc83ce64620523fb31
Ryan <ryantwalk@gmail.com>
2018-12-30 22:04:32 -0500

    add functionality to display owner record. #53.

--- petclinicweb/src/test/java/io/lker/petclinic/controllers/OwnerControllerTest.java
+++ petclinicweb/src/test/java/io/lker/petclinic/controllers/OwnerControllerTest.java
@@ -15,8 +15,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -65,5 +70,14 @@ class OwnerControllerTest {
 
         verifyZeroInteractions(ownerService);
 
+    }
+
+    @Test
+    void displayOwners() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id",is(1L))));
     }
 }
